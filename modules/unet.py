fa14067c9a3eaf718db487f7812c4b6b841f9ab2
Axel DINH VAN CHI <axeldvc@gmail.com>
2025-01-28 22:31:49 +0100

    3D Logging working

--- modules/unet.py
+++ modules/unet.py
@@ -7,8 +7,9 @@ from lightning import LightningModule
 from monai.losses import DiceCELoss
 from monai.networks.layers.factories import Act, Norm
 from monai.networks.nets import UNet
+from monai.visualize.utils import blend_images
 
-from utils.to_obj import array_to_obj
+from utils.to_obj import make_point_cloud
 
 
 class UNetModule(LightningModule):
@@ -45,6 +46,7 @@ class UNetModule(LightningModule):
             adn_ordering=adn_ordering,
         )
         self.loss_function = DiceCELoss(include_background=True, to_onehot_y=True)
+        self.lr = 3e-4
 
     def forward(self, image):
         return self.model(image)
@@ -64,15 +66,27 @@ class UNetModule(LightningModule):
         loss = self.loss_function(output, label)
         self.log("val_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
         prediction = torch.argmax(output, dim=1)
+        # Log a gif and a point cloud
         for i in range(prediction.shape[0]):
-            array_to_obj(
+            true_positives = (prediction[i] == label[i]).to(int) * label[i]
+            incorrect = (prediction[i] != label[i]).to(int) * (prediction[i] + label[i])
+            overlay = blend_images(image[i], true_positives, cmap="Greens")
+            overlay = blend_images(overlay, incorrect, cmap="Reds")
+            overlay = (overlay * 255).to(torch.uint8)
+            wandb.log(
+                {
+                    f"Prediction {i}": wandb.Video(
+                        overlay.permute(3, 0, 1, 2), fps=4, format="gif"
+                    )
+                }
+            )
+            vertices = make_point_cloud(
                 array=prediction[i].cpu().numpy(),
                 spacings=(1, 1, 1),
-                filename="tmp.obj",
+                color=(255, 0, 0),
             )
-            wandb.log({f"Predictions {i}": wandb.Object3D("tmp.obj")})
-            os.remove("tmp.obj")
+            wandb.log({f"Predictions {i}": wandb.Object3D(vertices)})
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=3e-4)
+        return torch.optim.Adam(self.model.parameters(), lr=self.lr)
--- utils/to_obj.py
+++ utils/to_obj.py
@@ -1,11 +1,36 @@
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
 import trimesh
 
+def make_point_cloud(array: np.ndarray, spacings: List[float], color=None) -> np.array:
 
-def array_to_obj(array: np.ndarray, spacings: List[float], filename: str):
+    array = array.astype(np.uint8)
+    depth = array.shape[-1]
+    spacings = np.array(spacings)
+
+    vertices = []
+    for i in range(depth):
+        contours = cv2.findContours(
+            array[..., i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )[0]
+        for contour in contours:
+            contour = contour[:, 0]
+            for point in contour:
+                vertex = [point[0], point[1], i]
+                if color is not None:
+                    vertex += list(color)
+                vertex = np.array(vertex).astype(float)
+                vertex[:3] *= spacings
+                vertices.append(vertex)
+
+    if len(vertices) > 0:
+        vertices = np.stack(vertices)
+
+    return vertices
+
+def array_to_obj(array: np.ndarray, spacings: List[float], color: Tuple[int], filename: str):
     """
     Convert a NumPy array to an object file.
 
@@ -15,30 +40,12 @@ def array_to_obj(array: np.ndarray, spacings: List[float], filename: str):
         The NumPy array to be converted, should be of shape (Width, Height, Depth).
     spacings : List[float]
        The spacing between elements in each dimension of the array.
+    color: Tuple[int]
+       The color of the elements in the array.
     filename : str
         The name of the output object file.
 
     """
-
-    array = array.astype(np.uint8)
-    depth = array.shape[-1]
-    spacings = np.array(spacings)
-
-    vertices = []
-    faces = []
-    for i in range(depth):
-        contours = cv2.findContours(
-            array[..., i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )[0]
-        for contour in contours:
-            contour = contour[:, 0]
-            for point in contour:
-                vertex = np.array([point[0], point[1], i])
-                vertex *= spacings
-                vertices.append(vertex)
-
-    if len(vertices) > 0:
-        vertices = np.stack(vertices)
-
-    mesh = trimesh.Trimesh(vertices, faces=faces)
+    vertices = make_point_cloud(array, spacings, color)
+    mesh = trimesh.Trimesh(vertices, faces=[])
     mesh.export(filename)
