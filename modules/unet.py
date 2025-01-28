64c5877c48c7cf80c67fcb099860d2941e9114d8
Axel DINH VAN CHI <axeldvc@gmail.com>
2025-01-29 00:26:02 +0100

    better overlays

--- modules/unet.py
+++ modules/unet.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import Dict, Sequence
 
 import torch
@@ -45,7 +46,7 @@ class UNetModule(LightningModule):
             bias=bias,
             adn_ordering=adn_ordering,
         )
-        self.loss_function = DiceCELoss(include_background=True, to_onehot_y=True)
+        self.loss_function = DiceCELoss(include_background=False, to_onehot_y=True)
         self.optimizer_config = optimizer_config
 
     def forward(self, image):
@@ -59,6 +60,9 @@ class UNetModule(LightningModule):
         self.log("loss", loss, prog_bar=True, on_step=True, on_epoch=True)
         return loss
 
+    def on_validation_epoch_start(self):
+        self.validation_step_outputs = []
+
     def validation_step(self, batch):
         image = batch["image"]
         label = batch["label"]
@@ -66,11 +70,28 @@ class UNetModule(LightningModule):
         loss = self.loss_function(output, label)
         self.log("val_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
         prediction = torch.argmax(output, dim=1)
-        # Log a gif and a point cloud
-        for i in range(prediction.shape[0]):
-            true_positives = (prediction[i] == label[i]).to(int) * label[i]
-            incorrect = (prediction[i] != label[i]).to(int) * (prediction[i] + label[i])
-            overlay = blend_images(image[i], true_positives, cmap="Greens")
+        self.validation_step_outputs.append(
+            {"prediction": prediction, "label": label, "image": image}
+        )
+
+        return prediction
+
+    def on_validation_epoch_end(self):
+        predictions = [item["prediction"] for item in self.validation_step_outputs]
+        labels = [item["label"] for item in self.validation_step_outputs]
+        images = [item["image"] for item in self.validation_step_outputs]
+
+        predictions = reduce(lambda x, y: x + y, [[x for x in b] for b in predictions])
+        labels = reduce(lambda x, y: x + y, [[x for x in b] for b in labels])
+        images = reduce(lambda x, y: x + y, [[x for x in b] for b in images])
+
+        for i in range(len(predictions)):
+            pred = predictions[i]
+            label = labels[i]
+            img = images[i]
+            true_positives = (pred == label).to(int) * label
+            incorrect = (pred != label).to(int) * (pred + label)
+            overlay = blend_images(img, true_positives, cmap="Greens")
             overlay = blend_images(overlay, incorrect, cmap="Reds")
             overlay = (overlay * 255).to(torch.uint8)
             wandb.log(
@@ -81,12 +102,11 @@ class UNetModule(LightningModule):
                 }
             )
             vertices = make_point_cloud(
-                array=prediction[i].cpu().numpy(),
+                array=pred.cpu().numpy(),
                 spacings=(1, 1, 1),
                 color=(255, 0, 0),
             )
             wandb.log({f"Point Cloud Validation {i}": wandb.Object3D(vertices)})
-        return loss
 
     def configure_optimizers(self):
         return getattr(torch.optim, self.optimizer_config["name"])(
